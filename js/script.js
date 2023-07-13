abe528e7eb972423777c67f63f2db92b5ac49963
AnonDEV23 <luisayvalballesteros@icloud.com>
2023-07-12 21:45:45 -0500

    fr

--- js/script.js
+++ js/script.js
@@ -1,38 +1,33 @@
 // Obtener elementos del DOM
 const taskForm = document.getElementById('task-form');
 const taskInput = document.getElementById('task-input');
+const addTaskBtn = document.getElementById('add-task-btn');
 const taskList = document.getElementById('task-list');
 
-// Evento de envío del formulario
-taskForm.addEventListener('submit', function(e) {
-    e.preventDefault();
-    
-    const taskText = taskInput.value.trim();
-    
-    if (taskText !== '') {
-        const taskItem = createTaskItem(taskText);
-        taskList.appendChild(taskItem);
-        taskInput.value = '';
-    }
-});
+// Agregar evento al botón de agregar tarea
+addTaskBtn.addEventListener('click', addTask);
 
-// Crear un nuevo elemento de tarea
-function createTaskItem(taskText) {
+// Agregar tarea a la lista
+function addTask() {
+  const taskText = taskInput.value;
+
+  if (taskText.trim() !== '') {
     const taskItem = document.createElement('li');
     taskItem.classList.add('task-item');
-    
-    const taskTextSpan = document.createElement('span');
-    taskTextSpan.textContent = taskText;
-    
-    const deleteButton = document.createElement('button');
-    deleteButton.textContent = 'Eliminar';
-    
-    deleteButton.addEventListener('click', function() {
-        taskItem.remove();
-    });
-    
-    taskItem.appendChild(taskTextSpan);
-    taskItem.appendChild(deleteButton);
-    
-    return taskItem;
+    taskItem.innerHTML = `
+      <span>${taskText}</span>
+      <button class="delete-btn">Eliminar</button>
+    `;
+    taskList.appendChild(taskItem);
+    taskInput.value = '';
+    addDeleteEvent(taskItem);
+  }
+}
+
+// Agregar evento para eliminar tarea
+function addDeleteEvent(taskItem) {
+  const deleteBtn = taskItem.querySelector('.delete-btn');
+  deleteBtn.addEventListener('click', () => {
+    taskItem.remove();
+  });
 }
