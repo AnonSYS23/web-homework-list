c523826d6d91d5f30f7a5f430a2d90e54373b84f
AnonDEV23 <luisayvalballesteros@icloud.com>
2023-07-14 17:50:59 -0500

    LOL

--- js/script.js
+++ js/script.js
@@ -1,33 +1,19 @@
-// Obtener elementos del DOM
-const taskForm = document.getElementById('task-form');
-const taskInput = document.getElementById('task-input');
-const addTaskBtn = document.getElementById('add-task-btn');
-const taskList = document.getElementById('task-list');
-
-// Agregar evento al botón de agregar tarea
-addTaskBtn.addEventListener('click', addTask);
-
-// Agregar tarea a la lista
-function addTask() {
-  const taskText = taskInput.value;
-
-  if (taskText.trim() !== '') {
-    const taskItem = document.createElement('li');
-    taskItem.classList.add('task-item');
-    taskItem.innerHTML = `
-      <span>${taskText}</span>
-      <button class="delete-btn">Eliminar</button>
-    `;
-    taskList.appendChild(taskItem);
-    taskInput.value = '';
-    addDeleteEvent(taskItem);
-  }
-}
-
-// Agregar evento para eliminar tarea
-function addDeleteEvent(taskItem) {
-  const deleteBtn = taskItem.querySelector('.delete-btn');
-  deleteBtn.addEventListener('click', () => {
-    taskItem.remove();
+$(document).ready(function() {
+  // Add task
+  $(".add-task").on("click", function() {
+    var task = $("<li><input type='checkbox' id='task'><label for='task'>New task</label></li>");
+    $(".tasks").append(task);
   });
-}
+
+  // Check/uncheck task
+  $(".tasks input").on("click", function() {
+    var task = $(this).closest(".task");
+    task.toggleClass("completed");
+  });
+
+  // Move task to later
+  $(".tasks .completed").on("click", function() {
+    var task = $(this).closest(".task");
+    task.appendTo(".later");
+  });
+});
