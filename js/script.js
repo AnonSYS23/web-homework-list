fd9044803a80af6f5ccf73ada02accbd37f88802
AnonDEV23 <luisayvalballesteros@icloud.com>
2023-07-18 18:54:24 -0500

    LOL

--- js/script.js
+++ js/script.js
@@ -1,4 +1,16 @@
-const input = document.getElementById("input");
-const btn_todo = document.getElementById("btn_todo");
-const container_todo = document.getElementById(".container_todo");
-let id=1
+$(function() {
+    // Add a new task when the "Add Task" button is clicked.
+    $('.btn-primary').on('click', function() {
+      // Get the task title from the input field.
+      var taskTitle = prompt('Enter a task title:');
+      // If the user entered a title, create a new task item and add it to the list.
+      if (taskTitle) {
+        var taskItem = $('<a href="#" class="list-group-item list-group-item-action">' +
+          '<h5 class="list-group-item-heading">' + taskTitle + '</h5>' +
+          '<p class="list-group-item-text">This is a task item.</p>' +
+          '</a>');
+        $('.list-group').append(taskItem);
+      }
+    });
+  });
+  
